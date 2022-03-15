f068d0039cc08100ef9d941b7c54f13dcebf53ae
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-15 16:28:25 +0100

    test

--- src/Middleware/AppendUnimpersonateButton.php
+++ src/Middleware/AppendUnimpersonateButton.php
@@ -3,12 +3,40 @@
 namespace DefStudio\Impersonator\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Blade;
 
 class AppendUnimpersonateButton
 {
     public function handle($request, Closure $next): mixed
     {
         $response = $next($request);
+
+        if(!Auth::user()?->is_impersonated()){
+            return $response;
+        }
+
+        $content = $response->getContent();
+
+        if(!$content){
+            return $response;
+        }
+
+        if(!str($content)->startsWith("<!DOCTYPE html>")){
+            return $response;
+        }
+
+        $before_body_close = str($content)->before('</body>');
+        $after_body_close = str($content)->after('</body>');
+
+        $button = Blade::render('impersonator:unimpersonate');
+
+        $content = $before_body_close
+            ->append($button)
+            ->append('</body>')
+            ->append($after_body_close);
+
+        $response->setContent((string)$content);
 
         return $response;
     }
