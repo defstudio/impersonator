7f497acd4771f712e1430f08d7e71cfa0ea8882d
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-15 12:45:19 +0100

    add impersonate trait

---
+++ src/Concerns/Impersonate.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DefStudio\Impersonator\Concerns;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * @mixin Authenticatable
+ * @mixin Model
+ */
+trait Impersonate
+{
+    public function can_impersonate(): bool
+    {
+        return true;
+    }
+
+    public function can_be_impersonated(): bool
+    {
+        return true;
+    }
+
+    public function is_impersonated(): bool
+    {
+        return !empty(session('impersonator'));
+    }
+
+    public function impersonator(): self|null
+    {
+        $impersonator_id = session('impersonator');
+
+        return self::find($impersonator_id);
+    }
+
+    public function impersonate(self $another_user): self
+    {
+        session()->invalidate();
+        session()->regenerateToken();
+
+        session()->regenerate();
+        Auth::login($another_user);
+
+        session()->put('impersonator', $this->id);
+
+        return $another_user;
+    }
+}
